eff61b08babb41cb6686c89fa685876e69a67b26
antirez <antirez@gmail.com>
2019-02-18 18:27:18 +0100

    Better distribution for set get-random-element operations.

--- src/dict.h
+++ src/dict.h
@@ -181,6 +181,7 @@ void dictResetIterator(dictIterator *iter);
 dictEntry *dictNext(dictIterator *iter);
 void dictReleaseIterator(dictIterator *iter);
 dictEntry *dictGetRandomKey(dict *d);
+dictEntry *dictGetFairRandomKey(dict *d);
 unsigned int dictGetSomeKeys(dict *d, dictEntry **des, unsigned int count);
 void dictGetStats(char *buf, size_t bufsize, dict *d);
 uint64_t dictGenHashFunction(const void *key, int len);
