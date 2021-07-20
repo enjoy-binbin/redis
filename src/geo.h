03626bbc1bff234fd52b5125d8f938f9b96b66ab
enjoy-binbin <binloveplay1314@qq.com>
2021-07-20 16:37:14 +0800

    geo

--- src/geo.h
+++ src/geo.h
@@ -6,17 +6,17 @@
 /* Structures used inside geo.c in order to represent points and array of
  * points on the earth. */
 typedef struct geoPoint {
-    double longitude;
-    double latitude;
-    double dist;
-    double score;
-    char *member;
+    double longitude; // 经度
+    double latitude; // 纬度
+    double dist; // 这个经纬度与另一个点之间的距离
+    double score; // 解码出经纬度的分值
+    char *member; // 分值对应的有序集合成员
 } geoPoint;
 
 typedef struct geoArray {
-    struct geoPoint *array;
-    size_t buckets;
-    size_t used;
+    struct geoPoint *array; // 用于储存多个地理位置的数组
+    size_t buckets; // 数组可用的项数量
+    size_t used; // 数组目前已用的项数量
 } geoArray;
 
 #endif
